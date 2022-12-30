5e24e232c5ae8362ded60e8e5f2d4ffa74f7d892
Bryan <bryanstevenal@gmail.com>
2022-12-30 22:35:09 +0100

    Modify index.ts in order to fix ProductView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import ProductsView from '../views/Products.vue';
+import ProductsView from '../views/ProductsView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
