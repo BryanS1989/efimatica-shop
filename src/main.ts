b1c4d162d1b45f4b9e3b4a1b43ff4fd53f6996ce
Bryan <bryanstevenal@gmail.com>
2022-12-31 12:08:07 +0100

    Setup Vuei18n

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,22 @@ import router from './router';
 
 import './assets/main.css';
 
+// Setup vueI18n
+import { createI18n } from 'vue-i18n';
+import localeES from './locale/es.json';
+
+const i18n = createI18n({
+    locale: 'es',
+    messages: {
+        es: localeES,
+    },
+});
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(i18n);
 
 app.mount('#app');
