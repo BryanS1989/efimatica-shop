442116d380b9ea6337f9ab1c193842dcd645e782
Bryan <bryanstevenal@gmail.com>
2023-01-01 15:32:31 +0100

    Use productItem component at productList

--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ProductItem from './ProductItem.vue';
+
+import { Product } from '../../interfaces/product.interface';
+
+export default defineComponent({
+    props: {
+        products: {
+            type: Array<Product>,
+            default() {
+                return [];
+            },
+        },
+    },
+    components: { ProductItem },
+    data() {
+        return {};
+    },
+    mounted() {},
+});
+</script>
 
 <template>
-    <article></article>
+    <article class="product__list flex">
+        <ProductItem
+            v-for="(product, i) in products"
+            :key="product.id"
+            :product="product"
+        ></ProductItem>
+    </article>
 </template>
