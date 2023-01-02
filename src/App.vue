2579f7ea3d713d763f0cdaaf2ba927b9201503c2
Bryan <bryanstevenal@gmail.com>
2023-01-02 09:41:45 +0100

    Use cartComponent

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import Header from './components/shared/headerComponent.vue';
+import CartComponent from './components/cart/CartComponent.vue';
 </script>
 
 <template>
     <Header></Header>
+
+    <CartComponent></CartComponent>
 
     <main>
         <RouterView />
